16a3c79ff8dfbc17bb47c5bc74c7db061b5b8463
Kaleb Zick <kalebzick@mail.weber.edu>
2025-02-06 20:02:29 -0700

    ADD: Docstring for program info

--- game.py
+++ game.py
@@ -1,2 +1,13 @@
+'''
+Adventrue Game
+Author: Kaleb Zick
+Version 1.0
+Description:
+This is a text-based adventure game where the player makes choices
+to navigate through a mysterious forest.
+'''
+
+
+# Welcome message and introduction 
 print("Welcome to the Adventure Game!")  
 print("Your journey begins here...")
