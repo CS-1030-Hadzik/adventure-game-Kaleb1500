a2b71433931be246ba522a9b849f174bdc0c7a0b
Kaleb Zick <kalebzick@mail.weber.edu>
2025-02-08 21:30:50 -0700

    ADD: player name, desisions and answers

--- game.py
+++ game.py
@@ -17,3 +17,25 @@ player_name = input("What is your name, adventurer?")
 
 # Concatenate strings to create a personalized message
 print ("Welcome," + player_name + "! Your journey begins now.")
+
+# Use an f-string to display the same message in a more readable way
+print(f"Welcome, {player_name}! Your journey begins now.")
+
+# Discribe the starting area
+starting_area = """
+You find yourself in a dark forest.
+The sound of rustling leaves fills the air.
+A faint path lies ahead, leading deeper into the unknown...
+"""
+print(starting_area)
+
+# ask the player for their fisrt dission
+deision = input("do you wish to take the path (yes or no): "). lower()
+
+# respond basied on player's dession 
+if deision =="yes": 
+    print(f"Brave choice, {player_name}! You step on the path and venture forward")
+elif deision =="no":
+    print(f"{player_name}, you decide to wait. Perhaps courage will come to you later.")
+else:
+    print("confused, you stand still, unsure of what to do")
